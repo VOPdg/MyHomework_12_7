4e020dd88881e7e5aeb4646d382c074d6985e8cc
veronikapodgol <veronika.podgol@gmail.com>
2022-04-18 09:30:23 +0300

    added selenide test

---
+++ src/test/java/guru/qa/SelenideTest.java
@@ -0,0 +1,22 @@
+package guru.qa;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+    @Test
+    public void testNameCheck(){
+        SelenideLogger.addListener("allure", new AllureSelenide()); //подключает слушателей
+        open("https://github.com/");
+        $(".header-search-input").sendKeys("VOPdg/MyHomework_ 12_7");
+        $(".header-search-input").submit();
+        $(By.linkText("VOPdg/MyHomework_12_7")).click();
+        $("#issues-tab").shouldHave(text("Issues"));
+    }
+}
---
+++ src/test/java/guru/qa/WebTest.java
@@ -0,0 +1,33 @@
+package guru.qa;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebTest {
+
+    @Step("Открываем гитхаб")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репо {repository}")
+    public void searchRepo(String repository) {
+        $(".header-search-input").sendKeys(repository);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Переходим в репо {repository}")
+    public void openRepoPage(String repository) {
+        $(By.linkText(repository)).click();
+    }
+
+    @Step("Проверяем название Issues в репо {repository}")
+    public void checkIssuesName(String repository) {
+        $("#issues-tab").shouldHave(text("Issues"));
+    }
+
+}
